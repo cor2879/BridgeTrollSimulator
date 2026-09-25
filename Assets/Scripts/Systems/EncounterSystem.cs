557566156ac6ffb6d9279c69992b04cce54ee0e9
David Cole <cor2879@gmail.com>
2026-09-24 23:26:09 -0400

    Trace WebGL encounter flow

--- Assets/Scripts/Systems/EncounterSystem.cs
+++ Assets/Scripts/Systems/EncounterSystem.cs
@@ -5,6 +5,7 @@ using OldSchoolGames.BridgeTrollSimulator.Scripts.Core.GameStateManagement;
 using OldSchoolGames.BridgeTrollSimulator.Scripts.Dialog;
 using OldSchoolGames.BridgeTrollSimulator.Scripts.Entities;
 using OldSchoolGames.BridgeTrollSimulator.Scripts.SocialDuel.Events;
+using OldSchoolGames.BridgeTrollSimulator.Scripts.Platform;
 
 namespace OldSchoolGames.BridgeTrollSimulator.Scripts.Systems
 {
@@ -15,6 +16,8 @@ namespace OldSchoolGames.BridgeTrollSimulator.Scripts.Systems
 
         private void OnEnable()
         {
+            WebGLDiagnostics.Trace("EncounterSystem.OnEnable");
+
             GameEventBus.Subscribe<EntityEncounterEvent>(OnEncounter);
             GameEventBus.Subscribe<DialogEndedEvent>(OnDialogEnded);
         }
@@ -31,6 +34,8 @@ namespace OldSchoolGames.BridgeTrollSimulator.Scripts.Systems
 
         private void OnEncounter(EntityEncounterEvent evt)
         {
+            WebGLDiagnostics.Trace($"EncounterSystem.OnEncounter {evt.Initiator?.SourceName} -> {evt.Target?.SourceName}");
+
             var initiator = (EntityController)evt.Initiator;
             var target = (NpcController)evt.Target;
 
@@ -67,6 +72,8 @@ namespace OldSchoolGames.BridgeTrollSimulator.Scripts.Systems
 
         private void StartDialog(EntityEncounterEvent evt)
         {
+            WebGLDiagnostics.Trace("EncounterSystem.StartDialog publishing DialogStartedEvent.");
+
             GameEventBus.Publish(
                 new DialogStartedEvent(
                     DialogSystem.Instance.DefaultEncounterNode,
